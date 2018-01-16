befbad7c76747b4144405565195e99ebe08a3843
Souloinc87 <souloinc87@gmail.com>
2018-01-16 04:20:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles() {
   var array = []
   var i = 0
-  do {array.push('I love the Beatles!')}
+  do {array.push('I love the Beatles!'); i++}
   while (i > 15)
-  i++
   return array
 }
