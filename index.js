696d5745f7e728a8529b9901132a7af7af3fe469
Souloinc87 <souloinc87@gmail.com>
2018-01-16 03:50:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + ' plays ' + instruments[i])
+
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  while ( i < facts.length) {
+    facts[i] += '!!!'
+    i++
+  }
+  return facts
+}
+
